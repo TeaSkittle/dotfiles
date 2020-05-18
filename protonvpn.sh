78095241b05326d123da654205b0986809c607fc
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-05-18 15:25:13 -0600

    Update protonvpn.sh

--- protonvpn.sh
+++ protonvpn.sh
@@ -2,8 +2,8 @@
 # Install and configure proton VPN (debian-based)
 sudo apt install -y openvpn dialog python3-pip python3-setuptools
 sudo pip3 install protonvpn-cli
-echo "Use OpenVPN account setting, not ProtonVPN."
-echo "Log into protonvpn.com to find OpenVPN account and pass"
+echo "Use OpenVPN account settings, not ProtonVPN."
+echo "Log into protonvpn.com to find OpenVPN account and password"
 sudo protonvpn init
 echo "Connecting to fastest VPN server..."
 sudo protonvpn c -f

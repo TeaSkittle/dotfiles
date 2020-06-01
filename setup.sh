78312642d58b896281c1addf07f6e1ec53fc3be1
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-06-01 15:18:31 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,30 +2,31 @@
 #
 # Packages
 #
-echo "[+]Installing system packages..."
+echo "[+] Installing system packages..."
 apt install -y \
 git stow net-tools whois pandoc scrot tldr dos2unix \
 emacs joe fish neofetch python3-pip python3-setuptools \
 htop vim shellcheck surf
-echo "[+]Installing pip packages"
+echo "[+] Installing pip packages"
 pip3 install speedtest-cli
-echo "[+]Updating system..."
+echo "[+] Updating system..."
 apt update
 #
 # Configurations
 #
-echo "[+]Moving .gitconfig to home..."
+echo "[+] Moving .gitconfig to home..."
 mv .gitconfig ~/.gitconfig
-echo "[+]Downloading and moving tron-legacy-theme.el ..."
+echo "[+] Downloading and moving tron-legacy-theme.el ..."
 curl -LO https://raw.githubusercontent.com/ianpan870102/tron-legacy-emacs-theme/master/tron-legacy-theme.el
 mkdir .emacs.d/themes/
 mv tron-legacy-theme.el .emacs.d/themes/
 #
-# Finalizing
+# Fish
 #
-echo "[+]Symlinking dotfiles via GNU Stow..."
-# may have the programs be arg variables
-stow emacs joe
+echo "[+] Setting fish as default shell..."
+which fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+# Complete
 echo "[+]Set up complete"
 
 

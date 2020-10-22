a2ee56ab13699d78a79d85eaefcf7b19d62a323d
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-10-22 11:08:07 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "[+] Installing system packages..."
 apt install -y \
 git stow net-tools whois pandoc scrot tldr dos2unix \
 emacs joe fish neofetch python3-pip python3-setuptools \
-htop vim shellcheck surf jed firefox-esr spectrwm xorg
+htop vim shellcheck surf jed firefox-esr spectrwm xorg mg
 echo "[+] Installing pip packages"
 pip3 install speedtest-cli
 echo "[+] Updating system..."

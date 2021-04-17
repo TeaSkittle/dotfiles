51bf18219ec4e7d9fac48d2d1ab89f214aef25b0
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2021-04-17 16:35:40 -0600

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,19 @@ alias em="emacs -nw"
 alias cls="clear"
 
 # Functions
-function cheat
+function cheat 
     curl -s cheat.sh/$argv
+end
+function scr 
+    ~/.scripts/$argv[1] $argv[2]
+end
+function "lscr" 
+    ls ~/.scripts/
+end
+function xlist
+    set_color green
+    tput bold
+    ls -al | grep -Ei '^[rwx-]{9}x' | awk '{print $9}'
+    tput sgr0
+    set_color normal
 end

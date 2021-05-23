fe60aeaaadd488a7597e14f5f0c52e5be3ff964b
TeaSkittle <tea@pop-os.localdomain>
2021-05-23 11:14:05 -0600

    Change font

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,11 @@
 # Disable fish greeting
 set fish_greeting
 
+# Custom prompt
+function fish_prompt
+    printf '%s%s%s λ ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
 # Aliases
 alias clock="date +'%Y-%m-%d %H:%M:%S'"
 alias rb="rm *~"
@@ -15,12 +20,15 @@ alias masscp="rsync -ah --progress"
 function cheat 
     curl -s cheat.sh/$argv
 end
+
 function scr 
     ~/.scripts/$argv[1] $argv[2]
 end
+
 function "lscr" 
     ls ~/.scripts/
 end
+
 function xlist
     set_color green
     tput bold
@@ -28,3 +36,4 @@ function xlist
     tput sgr0
     set_color normal
 end
+

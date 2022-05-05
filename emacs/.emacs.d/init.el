aefbb6cb688daca90020833880cfcd684755dac9
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-05-05 12:24:21 -0600

    Cleanup init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,64 +1,40 @@
-;;; Personal configuration -*- lexical-binding: t -*-
-;;; Based off of:
-;;;  https://github.com/susam/emfy
-;;;  https://emacs.amodernist.com/
+;;; -*- lexical-binding: t -*-
 
-;; Add the NonGNU ELPA package archive
+;; ====================
+;; Packages
+;; ====================
 (require 'package)
 (add-to-list 'package-archives  '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
-
-;; Install packages.
 (dolist (package '(markdown-mode rainbow-delimiters racket-mode mood-line))
   (unless (package-installed-p package)
     (package-install package)))
 
-;; Load a custom theme
-(load-theme 'wombat t)
-
-;; Custom cursor color
-(set-cursor-color "DeepPink")
-
-;; Disable the tool bar
-(tool-bar-mode -1)
-
-;; Disable splash screen
-(setq inhibit-startup-screen t)
-
-;; Mood-line
-(require 'mood-line)
-(mood-line-mode)
-
-;; Unbind Isnert Key
-(define-key global-map [(insert)] nil)
-
-;; Tab to 4 Space
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq tab-width 4)
-
-;; Gives right-click a context menu
-(global-set-key [mouse-3] 'mouse-popup-menubar-stuff)
-
-;; Enable line numbering by default
-(global-display-line-numbers-mode t)
-
-;; Enable column number in mode-line
-(column-number-mode)
+;; Start rainbow-delimiters
+(require 'rainbow-delimiters)
 
 ;; Ido Mode auto-completion
 (ido-mode 1)
 (ido-everywhere)
 (setq ido-enable-flex-matching t)
 
-;; Highlight matching pairs of parentheses.
-(setq show-paren-delay 0)
-(show-paren-mode)
+;; Mood-line
+(require 'mood-line)
+(mood-line-mode)
+
+;; ====================
+;; Visuals
+;; ====================
+(load-theme 'wombat t)               ; Load a custom theme
+(set-cursor-color "DeepPink")        ; Custom cursor color
+(tool-bar-mode -1)                   ; Disable the tool bar
+(setq inhibit-startup-screen t)      ; Disable splash screen
+(show-paren-mode)                    ; Show matching parens
+(setq show-paren-delay 0)            ; Paren match delay = 0
+(global-display-line-numbers-mode t) ; Enable line numbering by default
+(setq window-resize-pixelwise t)
+(setq frame-resize-pixelwise t) 
 
 ;; Customize Rainbow Delimiters
-(require 'rainbow-delimiters)
-
-;; Customize Rainbow Delimiters.
-(require 'rainbow-delimiters)
 (set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
 (set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
 (set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
@@ -69,16 +45,21 @@
 (set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
 (set-face-foreground 'rainbow-delimiters-depth-9-face "#666")  ; dark gray
 
-;; Visual adjustments
-(setq window-resize-pixelwise t)
-(setq frame-resize-pixelwise t) 
+;; ====================
+;; Functionality
+;; ====================
+(define-key global-map [(insert)] nil)                ; Unbind Isnert Key
+(global-set-key [mouse-3] 'mouse-popup-menubar-stuff) ; Gives right-click a context menu
 
-;; Miscellaneous options
-(setq-default major-mode
-              (lambda () ; guess major mode from file name
-                (unless buffer-file-name
-                  (let ((buffer-file-name (buffer-name)))
-                    (set-auto-mode)))))
+;; Tab to 4 Space
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq tab-width 4)
+
+
+;; ====================
+;; Miscellaneous
+;; ====================
 (savehist-mode t)                  ; Save minibuffer history over several sessions
 (recentf-mode t)                   ; Builds a list of recently opened files
 (defalias 'yes-or-no #'y-or-n-p)   ; y-or-n-p makes answering questions faster
@@ -88,18 +69,25 @@
 (delete-selection-mode 1)          ; Delete what is selected when typing
 (setq auto-save-default nil)       ; Stop creating #autosave# files
 (setq gc-cons-threshold 100000000) ; Garbage collection
+(setq-default major-mode
+              (lambda () ; guess major mode from file name
+                (unless buffer-file-name
+                  (let ((buffer-file-name (buffer-name)))
+                    (set-auto-mode)))))
 
-;; Write backups and savehist to separate directory
+;; ====================
+;; Files
+;; ====================
 (make-directory "/tmp/emacs/" t)
 (setq backup-directory-alist '(("." . "/tmp/emacs/")))
 (setq savehist-file "~/.emacs.d/savehist")
-
-;; Store automatic customization options elsewhere
 (setq custom-file (locate-user-emacs-file "custom.el"))
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; Start server
+;; ====================
+;; Server
+;; ====================
 (require 'server)
 (unless (server-running-p)
   (server-start))

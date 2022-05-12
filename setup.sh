6d3808ed82b277da890b6c31611888fc7e1192fa
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-05-12 15:40:28 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/sh
 echo "[+] Installing system packages..."
-apt install -y \
-git stow net-tools whois pandoc scrot tldr dos2unix \
-emacs joe fish neofetch python3-pip python3-setuptools \
-htop vim shellcheck surf jed firefox-esr spectrwm xorg mg
-echo "[+] Installing pip packages"
-pip3 install speedtest-cli
-echo "[+] Updating system..."
-apt update
-echo "[+] Setting up config files"
-cp .gitconfig ~/.gitconfig
+apt install -y git net-tools tldr emacs fish neofetch
 f=$(command -v fish | sudo tee -a /etc/shells)
 echo "[-] To change shell to fish: chsh -s $f"
 echo "[+] Set up complete"

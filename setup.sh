9053bc5bf8e932e5df165a63183b0ef76bd661f0
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-05-13 10:50:39 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,13 +9,14 @@ git clone https://github.com/TeaSkittle/dotfiles
 
 echo "[+] Copying files..."
 cd dotfiles/
-cp X11/.xprofile ~/.xprofile
-cp emacs/.emacs.d/init.el ~/.emacs.d/init.el 
-cp fish/.config/fish/config.fish ~/.config/fish/config.fish 
+mv X11/.xprofile ~/.xprofile
+mv emacs/.emacs.d/init.el ~/.emacs.d/init.el 
+mv fish/.config/fish/config.fish ~/.config/fish/config.fish 
 
-# Print fish command
+# Finish & Clean up
+echo "[+] Cleaning up files..."
+cd ../
+rm -rf dotfiles/
 f=$(command -v fish | sudo tee -a /etc/shells)
 echo "[!] To change shell to fish: chsh -s $f"
-echo ""
-echo "[+] Set up complete"
-echo ""
+echo -e "\n[+] Set up complete\n"

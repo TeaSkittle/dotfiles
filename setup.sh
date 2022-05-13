1fe0779a581cf5b1479efb2f66eded772d7814cd
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-05-13 09:00:04 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@
 echo "[+] Installing system packages..."
 apt install -y git net-tools tldr emacs fish neofetch
 
+echo "[+] Downloading repo..."
+git clone https://github.com/TeaSkittle/dotfiles
+
 echo "[+] Copying files..."
-
-# X11
+cd dotfiles/
 cp X11/.xprofile ~/.xprofile
-
-# Emacs
 cp emacs/.emacs.d/init.el ~/.emacs.d/init.el 
-
-# Fish
 cp fish/.config/fish/config.fish ~/.config/fish/config.fish 
+
+# Print fish command
 f=$(command -v fish | sudo tee -a /etc/shells)
 echo "[!] To change shell to fish: chsh -s $f"
 

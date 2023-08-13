8d3c278df46495f27441c26f1c20030307dfbe7f
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2023-08-13 08:37:04 -0600

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,14 +18,9 @@ alias xlist="ls -al | grep -Ei '^[rwx-]{9}x' | cut -c 48-"
 alias sx="startx"
 
 # Functions
-function kp -d "Stop PackageKit on OpenSUSE"
-    sudo systemctl stop packagekit.service
-end
-
 function owncat -d "Neatly print out file permissions"
     echo -n "Owner: "; ls -l $argv | awk '{print $3}'
     echo -n "Group: "; ls -l $argv | awk '{print $4}'
     echo -n "r/w/x: "; ls -l $argv | awk '{print $1}' | sed 's/#\\.//'
-        #echo "Octal: "; stat -c '%a %n' $argv | awk '{print $1}'
-        #^ Freebsd issue with stat commmand
+    echo "Octal: "; stat -c '%a %n' $argv | awk '{print $1}'
 end
